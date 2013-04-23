3d5838b2b8b681ca1d5d7a19d330be2d4e444b79
dkaai <debert@zagmail.gonzaga.edu>
2013-04-22 22:16:03 -0700

    Adds the Food and Meal classes to the package

---
+++ BiteCal/src/com/chewchew/bitecal/Food.java
@@ -0,0 +1,82 @@
+package com.chewchew.bitecal;
+
+public class Food 
+{
+    private String foodName;
+    private double  servings;
+    private int    calOneServing;
+    
+    public Food()
+    {
+        foodName = "carrot";
+        servings = -1;
+        calOneServing = -1; 
+    }
+    
+    public Food(String name, double serving, int cal)
+    {
+        foodName = name;
+        servings = serving;
+        calOneServing = cal;
+    }
+
+    public String getFoodName() 
+    {
+        return foodName;
+    }
+
+    public double getServings()
+    {
+        return servings;
+    }
+
+    public int getCalOneServing() 
+    {
+        return calOneServing;
+    }
+
+    public void setFoodName(String foodName)
+    {
+        this.foodName = foodName;
+    }
+
+    public void setServings(double servings)
+    {
+        this.servings = servings;
+    }
+
+    public void setCalOneServing(int calOneServing) 
+    {
+        this.calOneServing = calOneServing;
+    }
+    
+    //Returns the total calories of the entered food.
+    public int getFoodCalories(int calOneServing, double servings)
+    {
+        int foodCalories = (int) (calOneServing * servings);
+        return foodCalories;
+    }
+    
+    @Override
+    public String toString()
+    {
+        String info = "Food Name: " + foodName + 
+                "\nServings: " + servings + 
+                "\nCalories of 1 Serving: " + calOneServing + 
+                "\nTotal Calories of Food: " + this.getFoodCalories(calOneServing, servings);
+        return info;
+    }
+ 
+    //Returns the name, servings, and total calories of the food with spacing
+    public String info()
+    {
+        String cutServings = "" + servings;
+        if(cutServings.substring(cutServings.indexOf('.')).length() >= 3)
+        {
+            cutServings = cutServings.substring(0, cutServings.indexOf('.') + 3);
+        }
+        String info = "" + foodName + "   " + cutServings + "   " + this.getFoodCalories(calOneServing, servings);
+        return info;
+    }
+    
+}
---
+++ BiteCal/src/com/chewchew/bitecal/Meal.java
@@ -0,0 +1,70 @@
+package com.chewchew.bitecal;
+
+
+import java.util.ArrayList;
+
+public class Meal
+{
+    private ArrayList<Food> foods;
+    
+    public Meal()
+    {
+        foods = new ArrayList<Food>();
+    }
+    
+    public Meal(ArrayList<Food> food)
+    {
+        foods = food;
+    }
+
+    public ArrayList<Food> getFoods() 
+    {
+        return foods;
+    }
+
+    public void setFoods(ArrayList<Food> foods) 
+    {
+        this.foods = foods;
+    }
+    
+    //Returns the .info of food at given index.
+    public String returnFood(int index)
+    {
+        return foods.get(index).info();
+    }
+    
+    //Returns the .info of all foods in the meal.
+    public String returnFoods()
+    {
+        String totalFoods = "";
+        for(int i = 0; i < foods.size(); i++)
+        {
+            totalFoods += this.returnFood(i) + "\n";
+        }
+        return totalFoods;
+    }
+    
+    @Override 
+    public String toString()
+    {
+        return this.returnFoods();
+    }
+    
+    //Adds a given food to the meal
+    public void addFood(Food foodToAdd)
+    {
+        this.foods.add(foodToAdd);
+    }
+    
+    //Returns the total calories of the foods from the meal.
+    public int totalCal()
+    {
+        int tCal = 0;
+        for(int i = 0; i < foods.size(); i++)
+        {
+            tCal += foods.get(i).getFoodCalories(foods.get(i).getCalOneServing(), foods.get(i).getServings());
+        }
+        return tCal;
+    }
+    
+}
